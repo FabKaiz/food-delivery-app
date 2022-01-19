9e94149c000b6dee95ea7057f28f712667015326
Fabien Chareun <cacrfabfr@gmail.com>
2022-01-19 15:04:17 +0100

    how it work setup with media queries

--- src/containers/howitwork/HowItWork.jsx
+++ src/containers/howitwork/HowItWork.jsx
@@ -3,8 +3,25 @@ import './howitwork.css'
 
 const HowItWork = () => {
   return (
-    <div>
-      <h2>how it work</h2>
+    <div className="howitwork__container">
+        <h1 className="mobile__title">How the app works</h1>
+
+      <div className="howitwork__line"></div>
+      <div className="howitwork__content">
+        <h1>How the app works</h1>
+        <div className="howitwork__content-screenshot">
+          <p>picture</p>
+          <p>title</p>
+        </div>
+        <div className="howitwork__content-screenshot">
+          <p>picture</p>
+          <p>title</p>
+        </div>
+        <div className="howitwork__content-screenshot">
+          <p>picture</p>
+          <p>title</p>
+        </div>
+      </div>
     </div>
   )
 }
