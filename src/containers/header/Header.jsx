5981e8c8a461e25776918593eb361179bd976dae
Fabien Chareun <cacrfabfr@gmail.com>
2022-01-19 14:36:00 +0100

    header mediea queries

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import './header.css'
 
-const header = () => {
+
+import twoPhone from '../../assets/header-twophones.png'
+
+const Header = () => {
   return (
-    <div>
-      <h2>Header</h2>
+    <div className="header__container">
+      {/* <img src={headerImg} alt="food" /> */}
+
+      <div className="header__content">
+        <h3>Food app</h3>
+        <h1>Why stay hungry when you can order form Bella Onojie</h1>
+        <h4>Download the bella onoje’s food app now on</h4>
+        <div className="button__container">
+          <p className="button__header">Playstore</p>
+          <p className="button__header no__bg">App store</p>
+        </div>
+      </div>
+
+      <div className="header__phones">
+        <img src={twoPhone} alt="app on phone" className="two__phone" />
+      </div>
+
     </div>
   )
 }
 
-export default header
+export default Header
