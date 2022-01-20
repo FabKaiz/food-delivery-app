b78e31cc0958bbdbe33cb320af8fa9434f5cbd84
Fabien Chareun <cacrfabfr@gmail.com>
2022-01-20 12:04:22 +0100

    download setup css

--- src/containers/download/Download.jsx
+++ src/containers/download/Download.jsx
@@ -3,8 +3,13 @@ import './download.css'
 
 const Download = () => {
   return (
-    <div>
-      <h2>Download</h2>
+    <div className="download__container">
+      <h2>Download the app now.</h2>
+      <h4>Available on your favorite store. Start your premium experience now</h4>
+      <div className="button__container">
+        <p className="download__btn">Playstore</p>
+        <p className="download__btn no__bg">App store</p>
+      </div>
     </div>
 
   )
