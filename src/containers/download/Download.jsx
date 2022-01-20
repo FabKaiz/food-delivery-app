1e70eba1017c04874982ec7a52e9392810fa9caa
Fabien Chareun <cacrfabfr@gmail.com>
2022-01-20 12:19:44 +0100

    download section down with media queries

--- src/containers/download/Download.jsx
+++ src/containers/download/Download.jsx
@@ -7,8 +7,8 @@ const Download = () => {
       <h2>Download the app now.</h2>
       <h4>Available on your favorite store. Start your premium experience now</h4>
       <div className="button__container">
-        <p className="download__btn">Playstore</p>
-        <p className="download__btn no__bg">App store</p>
+        <p className="download__btn">Buy now</p>
+        <p className="download__btn no__bg">Try for free</p>
       </div>
     </div>
 
