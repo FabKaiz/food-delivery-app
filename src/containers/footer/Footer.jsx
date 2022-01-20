7bed60df4eb80ff87c5e81a9c62e0d8649d82b53
Fabien Chareun <cacrfabfr@gmail.com>
2022-01-20 13:47:00 +0100

    footer done with media queries

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -4,16 +4,19 @@ import './footer.css'
 import { FiFacebook } from 'react-icons/fi';
 import { BsTwitter, BsInstagram  } from "react-icons/bs";
 
+import logo from '../../assets/logo.png'
+
 const Footer = () => {
   return (
       <div className="footer">
         <div className="footer__container">
-          <img src='logo' alt="logo" className="logo__grey" />
+          <img src={logo} alt="logo" className="logo__grey" />
           <div className="footer__socials">
             <FiFacebook />
             <BsTwitter />
             <BsInstagram />
           </div>
+          <p>Copywright 2020 Bella Onojie.com</p>
         </div>
       </div>
   )
